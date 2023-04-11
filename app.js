67f2c8d55bd7b3b916aa3cde87ae25c759ef7913
shamshinmax <94185311+shamshinmax@users.noreply.github.com>
2023-04-12 00:19:42 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,9 +9,13 @@ let btn = document.getElementById("btn");
 
 var seltheme = document.getElementById("selectID");
 console.log(seltheme)
+
+document.getElementById('selectID').addEventListener('change', function() {
+  const n = this.value;
+})
 btn.addEventListener("click", function(){
     
     tg.MainButton.setText("ok");
     tg.MainButton.show();
-    tg.sendData(seltheme);
+    tg.sendData(n);
 });
