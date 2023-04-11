abec4f87905e87507dff1f8905cd34bda7d3cb2e
shamshinmax <94185311+shamshinmax@users.noreply.github.com>
2023-04-11 23:44:46 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#FF00FF";
 
 let btn = document.getElementById("btn");
-var seltheme = document.getElementById("selectID").value;
 
+var seltheme = document.getElementById("selectID").text;
+console.log(seltheme)
 btn.addEventListener("click", function(){
     
     tg.MainButton.setText("ok");
