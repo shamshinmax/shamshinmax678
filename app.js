e55ac6b95f9fb5864330b21fc578e9afa9e9697b
shamshinmax <94185311+shamshinmax@users.noreply.github.com>
2023-04-11 23:19:08 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ tg.MainButton.color = "#FF00FF";
 let btn = document.getElementById("btn");
 
 btn.addEventListener("click", function(){
-    tg.MainButton.setText("Сообщение отправлено!");
+    tg.MainButton.setText("ok");
     tg.MainButton.show();
-    tg.sendData("sendTestMessage");
+    tg.sendData("Заявка отправлена");
 });
