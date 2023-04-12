fe7c3f7b4bed95eac5307d5e91c1bbb905bbe6ad
shamshinmax <94185311+shamshinmax@users.noreply.github.com>
2023-04-12 22:12:04 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const times = document.getElementById("startTime");
 let btn = document.getElementById("btn");
-var time = 0
+var time = 1;
 startTime.addEventListener("input", () => {
     time = startTime.value;
-    console.log(time)
+    console.log(time);
 }, false);
 btn.addEventListener("click", function(){
     console.log(times);
