ca56c99d045c56bc737e843ea3b879de1a8e091e
shamshinmax <94185311+shamshinmax@users.noreply.github.com>
2023-04-12 21:54:05 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,15 +7,13 @@ tg.MainButton.color = "#FF00FF";
 
 let btn = document.getElementById("btn");
 
-var seltheme = document.getElementById("selectID");
-console.log(seltheme)
 
-document.getElementById('selectID').addEventListener('change', function() {
-  const n = this.value;
-})
+var time = document.getElementById("startTime").value;
+
+
 btn.addEventListener("click", function(){
     
     tg.MainButton.setText("ok");
     tg.MainButton.show();
-    tg.sendData(n);
+    tg.sendData(time);
 });
