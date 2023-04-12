bb911108e9e65b758879340a55220bda6071f165
shamshinmax <94185311+shamshinmax@users.noreply.github.com>
2023-04-12 22:18:05 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const times = document.getElementById("startTime");
+let tg = window.Telegram.WebApp;
+console.log(times);
+tg.expand();
+
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "#FF00FF";
+
 let btn = document.getElementById("btn");
 var time = 1;
+
 startTime.addEventListener("input", () => {
     time = startTime.value;
-    console.log(time);
 }, false);
+
+
 btn.addEventListener("click", function(){
-    console.log(times);
     
+    tg.MainButton.setText("ok");
+    tg.MainButton.show();
+    tg.sendData(time);
 });
